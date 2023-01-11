f0739bc111082dde72a3a59ea042eb196627b886
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2023-01-11 18:03:06 -0300

    req2 concluído

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import logo from './trivia.png';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Game from './pages/Game';
 import Login from './pages/Login';
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <br />
-        <Login />
-      </header>
-
+      <Switch>
+        <Route exact path="/" component={ Login } />
+        <Route path="/game" component={ Game } />
+      </Switch>
     </div>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import logo from '../trivia.png';
 
 class Login extends Component {
   state = {
@@ -24,45 +26,62 @@ class Login extends Component {
     }, this.loginValidation);
   };
 
+  handleClickPlay = async () => {
+    const { history } = this.props;
+    const response = await fetch('https://opentdb.com/api_token.php?command=request');
+    const json = await response.json();
+    localStorage.setItem('token', json.token);
+    history.push('/game');
+  };
+
   render() {
     const { nameInput, emailInput, isDisabled } = this.state;
 
     return (
-      <div>
-        <label htmlFor="nameInput">
-          <input
-            type="text"
-            id="nameInput"
-            name="nameInput"
-            data-testid="input-player-name"
-            placeholder="Nome"
-            value={ nameInput }
-            onChange={ this.handleChange }
-          />
-        </label>
+      <header className="App-header">
+        <img src={ logo } className="App-logo" alt="logo" />
         <br />
-        <label htmlFor="emailInput">
-          <input
-            type="email"
-            id="emailInput"
-            name="emailInput"
-            data-testid="input-gravatar-email"
-            placeholder="Email"
-            value={ emailInput }
-            onChange={ this.handleChange }
-          />
-        </label>
-        <br />
-        <button
-          type="button"
-          disabled={ isDisabled }
-          data-testid="btn-play"
-        >
-          Play
-        </button>
-      </div>
+        <div>
+          <label htmlFor="nameInput">
+            <input
+              type="text"
+              id="nameInput"
+              name="nameInput"
+              data-testid="input-player-name"
+              placeholder="Nome"
+              value={ nameInput }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <br />
+          <label htmlFor="emailInput">
+            <input
+              type="email"
+              id="emailInput"
+              name="emailInput"
+              data-testid="input-gravatar-email"
+              placeholder="Email"
+              value={ emailInput }
+              onChange={ this.handleChange }
+            />
+          </label>
+          <br />
+          <button
+            type="button"
+            disabled={ isDisabled }
+            data-testid="btn-play"
+            onClick={ () => this.handleClickPlay() }
+          >
+            Play
+          </button>
+        </div>
+      </header>
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape(PropTypes.any.isRequired).isRequired,
+};
 
 export default Login;
