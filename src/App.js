67e4ec7c240eb618e225ed8fdd094b5c940d7bd2
Bernardo Martorano <110858573+bermartorano@users.noreply.github.com>
2023-01-11 17:21:30 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ export default function App() {
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
         <br />
-        <LoginPage />
+        <Login />
       </header>
 
     </div>
