b9a6d7b13d69f46de1d2b82a1776ed78def8db9d
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2023-01-11 16:40:27 -0300

    req1 concluído

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import logo from './trivia.png';
 import './App.css';
+import LoginPage from './pages/LoginPage';
 
 export default function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={ logo } className="App-logo" alt="logo" />
-        <p>SUA VEZ</p>
+        <br />
+        <LoginPage />
       </header>
+
     </div>
   );
 }
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+
+class LoginPage extends Component {
+  state = {
+    nameInput: '',
+    emailInput: '',
+    isDisabled: true,
+  };
+
+  loginValidation = () => {
+    const { nameInput, emailInput } = this.state;
+    const nameIsValid = nameInput !== '';
+    const emailIsValid = emailInput !== '';
+
+    this.setState({
+      isDisabled: !(nameIsValid && emailIsValid),
+    });
+  };
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    }, this.loginValidation);
+  };
+
+  render() {
+    const { nameInput, emailInput, isDisabled } = this.state;
+
+    return (
+      <div>
+        <label htmlFor="nameInput">
+          <input
+            type="text"
+            id="nameInput"
+            name="nameInput"
+            data-testid="input-player-name"
+            placeholder="Nome"
+            value={ nameInput }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <br />
+        <label htmlFor="emailInput">
+          <input
+            type="email"
+            id="emailInput"
+            name="emailInput"
+            data-testid="input-gravatar-email"
+            placeholder="Email"
+            value={ emailInput }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <br />
+        <button
+          type="button"
+          disabled={ isDisabled }
+          data-testid="btn-play"
+        >
+          Play
+        </button>
+      </div>
+    );
+  }
+}
+
+export default LoginPage;
