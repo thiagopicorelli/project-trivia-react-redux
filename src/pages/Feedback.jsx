6482613df498d234cd0ac708ef57ba12d28fe818
Gabriel Aguiar Dantas <gabrielaguiardantas@yahoo.com.br>
2023-01-13 19:15:51 -0300

    req19 concluído, faltando somente os de testes

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { MD5 } from 'crypto-js';
 import Header from '../components/Header';
 
 class Feedback extends Component {
+  componentDidMount() {
+    const { name, score, email } = this.props;
+    const hash = MD5(email).toString();
+    const picture = `https://www.gravatar.com/avatar/${hash}`;
+    const playerRanking = {
+      name,
+      score,
+      picture,
+    };
+    if (localStorage.getItem('ranking') === null) {
+      localStorage.setItem('ranking', JSON.stringify([]));
+    }
+    const playersArray = JSON.parse(localStorage.getItem('ranking'));
+    const ranking = [...playersArray, playerRanking];
+    const orderedPlayers = ranking.sort((a, b) => b.score - a.score);
+    localStorage.setItem('ranking', JSON.stringify(orderedPlayers));
+  }
+
   feedbackMessage = () => {
     const { assertions } = this.props;
     const maxCorrectPhrase = 3;
@@ -55,6 +74,8 @@ Feedback.propTypes = {
   history: PropTypes.shape(PropTypes.any.isRequired).isRequired,
   score: PropTypes.number.isRequired,
   assertions: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  email: PropTypes.string.isRequired,
 };
 
 export default connect(mapStateToProps)(Feedback);
--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 class Ranking extends Component {
   render() {
     const { history } = this.props;
+    const ranking = JSON.parse(localStorage.getItem('ranking'));
 
     return (
       <div>
@@ -15,6 +17,21 @@ class Ranking extends Component {
         >
           Play Again
         </button>
+        {
+          ranking.map((player, index) => (
+            <div key={ index }>
+              <img
+                src={ player.picture }
+                alt="imagem de jogador"
+              />
+              <p data-testid={ `player-name-${index}` }>
+                {player.name}
+              </p>
+              <p data-testid={ `player-score-${index}` }>
+                {player.score}
+              </p>
+            </div>))
+        }
       </div>
     );
   }
@@ -24,4 +41,4 @@ Ranking.propTypes = {
   history: PropTypes.shape(PropTypes.any.isRequired).isRequired,
 };
 
-export default Ranking;
+export default connect()(Ranking);
