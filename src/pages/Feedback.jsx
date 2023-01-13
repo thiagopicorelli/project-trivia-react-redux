c59f4cbc59f6f6ea06e56831bde0e4f00570bf3d
Bernardo Martorano <bernardormartorano@gmail.com>
2023-01-13 17:18:01 -0300

    Requisito 14

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -5,11 +5,17 @@ import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
-    const { history } = this.props;
+    const { history, score, assertions } = this.props;
     return (
       <div>
         <Header />
         <p data-testid="feedback-text">Olá</p>
+        <p data-testid="feedback-total-score">{score}</p>
+        <p
+          data-testid="feedback-total-question"
+        >
+          {assertions}
+        </p>
         <button
           type="button"
           data-testid="btn-play-again"
@@ -33,10 +39,13 @@ const mapStateToProps = (state) => ({
   email: state.player.gravatarEmail,
   name: state.player.name,
   score: state.player.score,
+  assertions: state.player.assertions,
 });
 
 export default connect(mapStateToProps)(Feedback);
 
 Feedback.propTypes = {
   history: PropTypes.shape(PropTypes.any.isRequired).isRequired,
+  score: PropTypes.number.isRequired,
+  assertions: PropTypes.number.isRequired,
 };
