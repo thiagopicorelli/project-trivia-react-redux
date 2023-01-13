f3fdd3e4556ea3a672dd8a24d5f3a4368921b7a8
Pedro <pedrohalmeidamendonca@gmail.com>
2023-01-12 22:09:58 -0300

    comitando alterações

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -3,39 +3,87 @@ import { connect } from 'react-redux';
 import { MD5 } from 'crypto-js';
 import PropTypes from 'prop-types';
 import { fetchQuestions } from '../services';
+import { addScore } from '../redux/actions';
 
 class Game extends Component {
   state = {
     requestQuestions: [],
     currentQuestion: 0,
     loading: true,
+    timedOut: false,
+    timer: 30,
   };
 
   async componentDidMount() {
     const { history } = this.props;
+    const shuffleNumber = 0.5;
     const fetchedQuestions = await fetchQuestions();
     const errorCode = 3;
     if (fetchedQuestions.response_code === errorCode) {
       localStorage.removeItem('token');
       history.push('/');
     }
-    this.setState({
-      requestQuestions: fetchedQuestions.results,
-      loading: false,
+    fetchedQuestions.results.forEach((questionComponent) => {
+      const answers = [
+        ...questionComponent.incorrect_answers,
+        questionComponent.correct_answer,
+      ];
+
+      const answersObject = answers.map((answer, index) => ({ text: answer, index }));
+      questionComponent.answers = answersObject.sort(() => Math.random() - shuffleNumber);
     });
+
+    this.setState(
+      {
+        requestQuestions: fetchedQuestions.results,
+        loading: false,
+      },
+      this.setTimer,
+    );
   }
+
+  async setTimer() {
+    const s = 1000;
+    const questionTimer = 30000;
+    setInterval(() => {
+      const { timer } = this.state;
+      this.setState({ timer: timer - 1 });
+    }, s);
+    setTimeout(() => {
+      this.setState({ timedOut: true });
+    }, questionTimer);
+  }
+
+  handleClick = ({ target }) => {
+    const { dispatch, score } = this.props;
+    const { requestQuestions, currentQuestion, timer } = this.state;
+    if (target.innerText === requestQuestions[currentQuestion].correct_answer) {
+      const easy = 1;
+      const medium = 2;
+      const hard = 3;
+      const baseScore = 10;
+
+      let difficultyLevel;
+      if (requestQuestions[currentQuestion].difficulty === 'easy') {
+        difficultyLevel = easy;
+      }
+      if (requestQuestions[currentQuestion].difficulty === 'medium') {
+        difficultyLevel = medium;
+      }
+      if (requestQuestions[currentQuestion].difficulty === 'hard') {
+        difficultyLevel = hard;
+      }
+      console.log(timer);
+      const totalScore = score + (baseScore + timer * difficultyLevel);
+      dispatch(addScore(totalScore));
+    }
+  };
 
   render() {
     const { email, name, score } = this.props;
-    const { requestQuestions, currentQuestion, loading } = this.state;
+    const { requestQuestions, currentQuestion, loading, timedOut } = this.state;
     const questionComponent = requestQuestions[currentQuestion];
     const hash = MD5(email).toString();
-    const shuffleNumber = 0.5;
-    let answersArray = [];
-    if (!loading) {
-      answersArray = [...questionComponent.incorrect_answers,
-        questionComponent.correct_answer];
-    }
 
     return (
       <div>
@@ -48,38 +96,31 @@ class Game extends Component {
           <p data-testid="header-player-name">{name}</p>
           <p data-testid="header-score">{score}</p>
         </header>
-        {
-          loading ? <h1>carregando...</h1> : (
-            <main>
-              <h4 data-testid="question-category">
-                {
-                  questionComponent.category
-                }
-              </h4>
-              <p data-testid="question-text">
-                {
-                  questionComponent.question
-                }
-              </p>
-              <div data-testid="answer-options">
-                {
-                  answersArray
-                    .map((answers, index) => (
-                      <button
-                        key={ index }
-                        type="button"
-                        data-testid={ answersArray.length - 1 === index
-                          ? 'correct-answer' : `wrong-answer-${index}` }
-                      >
-                        {answers}
-                      </button>
-                    )).sort(() => Math.random() - shuffleNumber)
-                }
-              </div>
-            </main>
-          )
-        }
-
+        {loading ? (
+          <h1>carregando...</h1>
+        ) : (
+          <main>
+            <h4 data-testid="question-category">{questionComponent.category}</h4>
+            <p data-testid="question-text">{questionComponent.question}</p>
+            <div data-testid="answer-options">
+              {questionComponent.answers.map((answers, index) => (
+                <button
+                  key={ index }
+                  type="button"
+                  data-testid={
+                    questionComponent.answers.length - 1 === answers.index
+                      ? 'correct-answer'
+                      : `wrong-answer-${answers.index}`
+                  }
+                  disabled={ timedOut }
+                  onClick={ this.handleClick }
+                >
+                  {answers.text}
+                </button>
+              ))}
+            </div>
+          </main>
+        )}
       </div>
     );
   }
@@ -96,6 +137,7 @@ Game.propTypes = {
   name: PropTypes.string.isRequired,
   score: PropTypes.number.isRequired,
   history: PropTypes.shape(PropTypes.any.isRequired).isRequired,
+  dispatch: PropTypes.func.isRequired,
 };
 
 export default connect(mapStateToProps)(Game);
